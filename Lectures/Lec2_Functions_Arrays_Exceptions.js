618082d5dca2f51216cb6ac6129b41a961970820
Marta Horbatska <marta.horbatska@vivint.com>
2025-03-09 14:10:08 -0600

    Lec2_task5

--- Lectures/Lec2_Functions_Arrays_Exceptions.js
+++ Lectures/Lec2_Functions_Arrays_Exceptions.js
@@ -361,37 +361,127 @@ console.log(processData(10, 4, "product")); // 40
 console.log(processData(10, 4, "square")); // 10000
 console.log(processData(10, 4, "time")); //Unknown action
 
-/*
-Task 2. Реалізуйте функцію findElem(arr, el), яка приймає параметри arr - масив і el - елемент. 
-Функція має визначити чи міститься елемент el в масиві arr, і повертати відповідне булеве значення.
 
+/* Task 2. Реалізуйте функцію findElem(arr, el), яка приймає параметри arr - масив і el - елемент. 
+Функція має визначити чи міститься елемент el в масиві arr, і повертати відповідне булеве значення.
 Приклад роботи:
 const arr = [1, 2, 3, 4, "Alex", 10, "Nick"];
 console.log(findElem(arr, 7)); // false
 console.log(findElem(arr, "Alex")); // true
+ */
+
+const arr = [1, 2, 3, 4, "Alex", 10, "Nick"];
+function findElem (arr, el){ 
+    return arr.includes(el);
+}
+console.log(findElem(arr, 7)); // false
+console.log(findElem(arr, "Alex")); // true
 
 
-
-Task 3. Реалізуйте функцію deleteItem(arr, item), яка приймає параметри arr - масив і item – елемент масиву. 
+/* Task 3. Реалізуйте функцію deleteItem(arr, item), яка приймає параметри arr - масив і item – елемент масиву. 
 Функція має видаляти елемент item з масиву arr, і повертати вихідний масив, який не містить цього елемента.
 Приклад роботи:
-console.log(deleteItem([3, 12, 16, 19, 21, 33], 16)); // [3, 12, 19, 21, 33]
+console.log(deleteItem([3, 12, 16, 19, 21, 33], 16)); // [3, 12, 19, 21, 33] */
 
-Task 4. Реалізуйте функцію getCircleLength(r), яка приймає параметр r - радіус кола і обчислює його довжину. 
+const arr1 = [3, 12, 16, 19, 21, 33];
+function deleteItem(arr1,item){
+const index = arr1.indexOf(item);
+index !== -1 && arr1.splice(index,1)
+return arr1;
+}
+console.log(deleteItem([3, 12, 16, 19, 21, 33], 16));
+
+
+// console.log(removeAll([1, 2, 3, 2, 4, 2, 5], 2)); 
+// // [1, 3, 4, 5]
+
+const days = ["mo", "tue", "we", "th", "fr"]; // додати елементи
+function addDays (arr){
+    arr.push ( "sut", "sun")
+    return arr;
+}
+console.log(addDays(days));
+
+function deleteAll(arr, item) { // видалити всі
+    return arr.filter(el => el !== item);
+}
+console.log(deleteAll(["mo", "tue", "we", "we", "fr"], "we"));
+// ["mo", "tue", "fr"]
+
+
+// ***************не змінює ориг масив - видаляє перуш і ост
+let days = ["mo", "tue", "we", "th", "fr"];   // 
+function removeFirstAndLast(arr) {
+arr.pop();  // видаляє останній ("fr")
+arr.shift(); // видаляє перший ("mo")
+return arr;
+}
+console.log(removeFirstAndLast(days)); 
+
+// // *************** змінює ориг масив - видаляє перуш і ост
+function removeFirstAndLastCopy(arr) {
+    return arr.slice(1, -1); // повертає копію без першого і останнього
+}
+let days = ["mo", "tue", "we", "th", "fr"];
+console.log(removeFirstAndLastCopy(days)); 
+console.log(days); 
+
+/* Task 4. Реалізуйте функцію getCircleLength(r), яка приймає параметр r - радіус кола і обчислює його довжину. 
 Передбачити припинення виконання програми і генерацію помилки у випадку, 
 якщо функції передано не числовий або менший-рівний 0 параметр.
 Напишіть код, який використовує цю функцію та обробляє можливі виняткові ситуації.
 Приклад роботи:
 getCircleLength(“five”); // “Incorrect radius - please, enter a positive numeric value!”
+ */
+function getCircleLength(r) {
+    if (typeof r !== "number" || isNaN(r) || r <= 0) {
+        throw new Error("Incorrect radius - please, enter a positive numeric value!");
+    }
+    return 2 * Math.PI * r;
+}
 
-Task 5. Реалізуйте функцію checkID(), яка пропонуватиме користувачу ввести власне ID. Валідні ID знаходяться в діапазоні від 1 до 1000.
+let input = prompt("Введіть радіус кола:");
+let radius = parseFloat(input); 
+
+try {
+    let length = getCircleLength(radius);
+    alert("Довжина кола: " + length); 
+} catch (error) {
+    alert(`Неправильне значення: "${input}": ${error.message}`); // тут ми виводимо повідомлення про помилку
+}
+
+
+/* Task 5. Реалізуйте функцію checkID(), яка пропонуватиме користувачу ввести власне ID. Валідні ID знаходяться в діапазоні від 1 до 1000.
 Функція має генерувати в модальному вікні помилки у випадку, коли:
 - юзер ввів порожню стрічку (наприклад “The field is empty! Please enter your ID”),
 - нечислове значення
 - ID знаходиться поза межами коректного діапазону.
 Якщо ID валідне юзер отримує відповідне сповіщення.
-В блокові catch передбачити виведення назви і опису помилки.
+В блокові catch передбачити виведення назви і опису помилки. */
 
+function checkId(id) {
+    if (id === "") {
+        throw new Error("The field is empty! Please enter your ID");
+    }
+    if (isNaN(id)) {
+        throw new Error("ID must be a number!");
+    }
+    if (id <= 0 || id > 1000) {
+        throw new Error("ID must be between 1 and 1000!")
+    }
+
+    alert(`Your ID (${id}) is valid.`);
+}
+let input = prompt("Введіть Id вашого документу: ");
+let idNumber = parseFloat(input);
+
+try {
+    checkId(input);
+} catch (error) {
+    alert(`${error.name}: ${error.message}`);
+}
+
+/*
 Task 6*. Реалізуйте функцію findArrDiff(arr1, arr2), яка приймає числові масиви arr1, 
 arr2 і повертає масив чисел, які не повторюються у вихідних масивах.
 Приклад роботи:
